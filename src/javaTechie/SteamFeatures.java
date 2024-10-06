6421c33bf9d074bd5a8787aa336ffbeb0c253de0
Dheeraj Kumar <dheeraj979216@gmail.com>
2024-10-07 00:18:30 +0530

    chore(0000):Java-8 stream features.

--- src/javaTechie/SteamFeatures.java
+++ src/javaTechie/SteamFeatures.java
@@ -1,6 +1,11 @@
 package javaTechie;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SteamFeatures {
+	public static void main(String[] args) {
 	/*
 	 * 1.Stream.ofNullable
 	 * 2.stream.iterator
@@ -11,8 +16,12 @@ public class SteamFeatures {
 	 * 7.collectores.partitioningBy
 	 * 8.IntStream for Range
 	*/
-	public static void main(String[] args) {
+		
+		
+		List<String> list = Arrays.asList("tim","pen",null,"sam",null,"bob","lily",null,"Alice","suzan");
+		List<String> listWithoutNull = list.stream().filter(l -> l!=null).collect(Collectors.toList());
 
+		System.out.println(listWithoutNull);
 	}
 
 }
